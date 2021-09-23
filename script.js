d07f5835f262b5943fde094cf34d04677daf1872
Code Foxx <iamcodefoxx@gmail.com>
2021-09-22 21:19:55 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
+setTimeout(function() { 
+  let modal= document.querySelector(".modal-wrapper");
+  modal.classList.add("showModal");
+}, 5000);
+
+ 
+document.querySelector(".close-modal").addEventListener("click", () => {
+  let modal = document.querySelector(".modal-wrapper");
+  modal.classList.remove("showModal");
+})
